cbbf08cc10cdde36c92e95bcfd4b90cb9151350b
Ramon Rodrigues <ramon.srodrigues01@gmail.com>
2020-06-20 15:17:25 -0300

    Changed debug mode

--- application/settings.py
+++ application/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["3.133.207.125"]
 
